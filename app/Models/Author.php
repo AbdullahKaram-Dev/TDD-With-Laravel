4334c1de4d90ca2a83e770e4ad3c508e5940a2f6
Abdullah Karam <abdallakaramdev@gmail.com>
2022-06-22 22:31:39 +0200

    make controller file and model file for author

---
+++ app/Models/Author.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Author extends Model
+{
+    use HasFactory;
+    protected $guarded = [];
+    /* example property casts */
+    protected $casts = [
+        'dob' => 'date',
+    ];
+
+    /* example property casts dates to date object */
+    protected $dates = [
+        'dob',
+    ];
+
+    /* another example to set attribute  dob */
+    public function setDobAttribute($dob)
+    {
+        $this->attributes['dob'] = Carbon::parse($dob);
+    }
+}
