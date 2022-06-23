ed8b3667bf549a5184dff296661a4e97c2c300d9
Abdullah Karam <abdallakaramdev@gmail.com>
2022-06-23 17:10:30 +0200

    create model book

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -9,4 +9,39 @@ class Book extends Model
 {
     use HasFactory;
     protected $guarded = [];
+
+    public function setAuthorIdAttribute($author)
+    {
+        $this->attributes['author_id'] = Author::firstOrCreate([
+            'name' => $author
+        ])->id;
+    }
+
+    public function reservations()
+    {
+        return $this->hasMany(Reservation::class);
+    }
+
+    public function checkin($user)
+    {
+        $reservation = $this->reservations()->where('user_id', $user->id)
+            ->whereNotNull('checked_out_at')
+            ->whereNull('checked_in_at')
+            ->first();
+        if (!$reservation) {
+            throw new \Exception('Book is not checked out');
+        }
+        $reservation->update([
+                'checked_in_at' => now()
+            ]);
+
+    }
+
+    public function checkout(User $user)
+    {
+        $this->reservations()->create([
+            'user_id' => $user->id,
+            'checked_out_at' => now(),
+        ]);
+    }
 }
